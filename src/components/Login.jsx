c69f63efc9e02aa7ffb63dcd313410dfee27cd38
XXX <muhiddinovhusan0207@gmail.com>
2024-04-07 05:51:26 +0500

    x

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,12 +29,12 @@ const Login = () => {
           <div className='mb-4'>
             <label htmlFor="username" className='form-label'>Username</label>
             <input type="text" className='form-control' placeholder='Username' id='username' {...register('username', { required: true })} />
-            {errors.username && <span className="text-danger">This field is required</span>}
+            {errors.username && <span className="text-danger">Username is required</span>}
           </div>
           <div className='mb-4'>
             <label htmlFor="password" className='form-label'>Password</label>
             <input type="password" className='form-control' placeholder='Password' id='password' {...register('password', { required: true })} />
-            {errors.password && <span className="text-danger">This field is required</span>}
+            {errors.password && <span className="text-danger">Password is required</span>}
           </div>
           <div className='text-center'>
             <button className='btn btn-outline-success w-50' type='submit' disabled={Object.keys(errors).length !== 0}>
